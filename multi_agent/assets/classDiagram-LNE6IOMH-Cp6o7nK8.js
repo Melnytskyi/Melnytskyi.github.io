977c35fae0f9b411767037738b6dfcf1c9517b25
Danyil Melnytskyi <ddann700@gmail.com>
2026-01-20 00:00:29 +0000

    Fixed UI for phones

--- multi_agent/assets/classDiagram-LNE6IOMH-Cp6o7nK8.js
+++
@@ -1,1 +0,0 @@
-import{s,c as e,a as r,b as l}from"./chunk-T2TOU4HS-C7QEqUpX.js";import{_ as t}from"./index-DFtftWzn.js";import"./chunk-5HRBRIJM-BIzHa4IR.js";var _={parser:l,db:r,renderer:e,styles:s,init:t(a=>{a.class||(a.class={}),a.class.arrowMarkerAbsolute=a.arrowMarkerAbsolute,r.clear()},"init")};export{_ as diagram};
